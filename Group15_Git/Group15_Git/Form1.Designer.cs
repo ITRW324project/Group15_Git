0ab8b3d74129356e070c3776bba0e66cad52829a
Dehan <dehanduvenhage@gmail.com>
2017-08-08 19:46:55 +0200

    Merge remote-tracking branch 'origin/develop' into develop
    Two merge issues solved

--- Group15_Git/Group15_Git/Form1.Designer.cs
+++ Group15_Git/Group15_Git/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Simeon";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -53,6 +54,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Stephan";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -62,6 +64,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Altus";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
