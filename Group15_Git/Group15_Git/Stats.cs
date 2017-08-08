988ef36a553831824bd992a9b6c522ab34429956
Simeon <simeon.went@gmail.com>
2017-08-08 21:16:55 +0200

    Changes made in Simeon form

--- Group15_Git/Group15_Git/Stats.cs
+++ Group15_Git/Group15_Git/Stats.cs
@@ -34,5 +34,22 @@ namespace Group15_Git
 
             return array[i / 2];
         }
+
+        public double calcMinimum(int i)
+        {
+            Random n = new Random();
+            int[] array = new int[i];
+            int smallest = 50;
+            for (int j = 0; j < i; j++)
+            {
+                array[j] = n.Next(10, 50);
+                smallest = array[j];
+                if (array[j] < smallest)
+                {
+                    smallest = array[j];
+                }
+            }
+            return smallest;
+        }
     }
 }
