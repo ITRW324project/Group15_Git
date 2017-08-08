2c293dce51cb02bf17e1067854d6667098f6592f
David <24522988@student.g.nwu.ac.za>
2017-08-08 21:21:23 +0200

    kode nou in stats

--- Group15_Git/Group15_Git/Stats.cs
+++ Group15_Git/Group15_Git/Stats.cs
@@ -51,5 +51,34 @@ namespace Group15_Git
             }
             return smallest;
         }
+        public double calcStdDev(int i)
+        {
+            Random n = new Random();
+            int[] array = new int[i];
+            int sum = 0;
+            int count = 0;
+            int smallest = 50;
+            for (int j = 0; j < i; j++)
+            {
+                count += j;
+                sum += array[j];
+                array[j] = n.Next(10, 50);
+                smallest = array[j];
+
+                if (array[j] < smallest)
+                {
+                    smallest = array[j];
+                }
+            }
+            double mean = sum / count;
+            double volg = 0;
+            for (int j = 0; j < i; j++)
+            {
+                count += j;
+                volg += Math.Sqrt(array[j] - mean);
+            }
+            double stndDev = volg / count;
+            return stndDev;
+        }
     }
 }
