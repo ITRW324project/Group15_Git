b7fd9b8336a5642c04ff1a0c922a9c6159cc8fbc
Jennifer Bester <besterjennifer5@gmail.com>
2017-08-08 21:26:00 +0200

    code in stats and test
    Test for between 5 and 20

--- Group15_Git/Group15_Git/Stats.cs
+++ Group15_Git/Group15_Git/Stats.cs
@@ -80,5 +80,14 @@ namespace Group15_Git
             double stndDev = volg / count;
             return stndDev;
         }
+
+        public double generate_number_J(int i)
+        {
+            Random ran = new Random();
+            int[] amount = new int[50];
+            for (int j = 0; j < i; j++)
+                amount[j] = ran.Next(1, 50);
+            return amount.Average();
+        }
     }
 }
