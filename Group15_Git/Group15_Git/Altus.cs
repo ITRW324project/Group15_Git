09adcb987b7cce0965c441cca4bd3c2eb9e2e775
Altus Minnie <altusminnie@gmail.com>
2017-08-08 21:07:33 +0200

    Revert "Try and Catch Code"
    This reverts commit b01fa08d5097cca4f75e79367a1a71e7ac8f5274.

--- Group15_Git/Group15_Git/Altus.cs
+++ Group15_Git/Group15_Git/Altus.cs
@@ -19,23 +19,8 @@ namespace Group15_Git
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try
-            {
-                int test = Int32.Parse(textBox2.Text);
-
-                Except exc = new Except();
-                if (exc.BetweenTest(Convert.ToInt32(textBox2.Text)) == true)
-                    MessageBox.Show("Between 5 and 20");
-                else
-                    MessageBox.Show("Not Between 5 and 20");
-                CalcCount c = new CalcCount();
-                MessageBox.Show(textBox2.Text);
-
-            }
-            catch
-            {
-                MessageBox.Show(textBox2.Text + " is not an integer, try again please !");
-            }
+            CalcCount count = new CalcCount();
+            MessageBox.Show(textBox2.Text);
         }
     }
 }
